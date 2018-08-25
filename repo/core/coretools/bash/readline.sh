7c955fac9977fb83daa79abda171527f769d12f5
itsmanjeet <sainishaab550@gmail.com>
2018-08-25 11:54:14 +0530

    repo

---
+++ repo/core/coretools/bash/readline.sh
@@ -0,0 +1,4 @@
+if [ -z "$INPUTRC" -a ! -f "$HOME/.inputrc" ] ; then
+        INPUTRC=/etc/inputrc
+fi
+export INPUTRC
---
+++ repo/core/libcore/bc/pre_fix.sh
@@ -0,0 +1,13 @@
+cat > bc/fix-libmath_h << "EOF"
+#! /bin/bash
+sed -e '1   s/^/{"/' \
+    -e     's/$/",/' \
+    -e '2,$ s/^/"/'  \
+    -e   '$ d'       \
+    -i libmath.h
+
+sed -e '$ s/$/0}/' \
+    -i libmath.h
+EOF
+
+sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure
